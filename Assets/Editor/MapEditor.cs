a703834ba951e4c38766e4cf83cd814e28133198
nader-naderi <73893373+nader-naderi@users.noreply.github.com>
2021-01-07 03:27:39 +0330

    Editor/Tilemap
    i made a tilemap and a editor for it.

---
+++ Assets/Editor/MapEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace NDRSims.TileMap
+{
+    [CustomEditor(typeof(MapGenerator))]
+    public class MapEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            MapGenerator map = target as MapGenerator;
+            map.GenerateMap();
+        }
+    }
+}
--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -16,19 +16,36 @@ namespace NDRSims
         {
             Vector3 pos = transform.position;
 
-            if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+            //if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+            //{
+            //    pos.z += panSpeed * Time.deltaTime;
+            //}
+            //if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+            //{
+            //    pos.z -= panSpeed * Time.deltaTime;
+            //}
+            //if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+            //{
+            //    pos.x += panSpeed * Time.deltaTime;
+            //}
+            //if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+            //{
+            //    pos.x -= panSpeed * Time.deltaTime;
+            //}
+
+            if (Input.GetKey("w"))
             {
                 pos.z += panSpeed * Time.deltaTime;
             }
-            if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+            if (Input.GetKey("s") )
             {
                 pos.z -= panSpeed * Time.deltaTime;
             }
-            if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+            if (Input.GetKey("d") )
             {
                 pos.x += panSpeed * Time.deltaTime;
             }
-            if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+            if (Input.GetKey("a") )
             {
                 pos.x -= panSpeed * Time.deltaTime;
             }
